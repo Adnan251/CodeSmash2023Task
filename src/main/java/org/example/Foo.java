eb888511c146737e106544e20b9a8ab6b2e9d806
Adnan251 <92021983+Adnan251@users.noreply.github.com>
2023-04-19 01:36:05 +0200

    Add files via upload

---
+++ src/main/java/org/example/Foo.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Foo {
+    public static int IsBuyerWinner(List<String> codeList, List<String> shoppingCart) {
+
+        if(codeList.isEmpty()) {
+            return 1;
+        }
+
+        int justhold = 0;   //used to check if we found a match
+        int codeTracker = 0; //tracks which element from codeList we are checking
+        int shoppingTracker = 0; //tracks which element from shoppingcart list we are checking
+
+        while(codeTracker < codeList.size()){  // loop through codeList
+
+            List<String> list = convertCorrectly(codeList.get(codeTracker)); //convert string in codeList to a list
+            int i = 0;
+            int j = shoppingTracker;
+
+            while(j < shoppingCart.size()){   //check if all the elements from new list are in shoppingCart in order
+                if(list.get(i).equals(shoppingCart.get(j)) || list.get(i).equals("anything")) {
+                    i++;
+                    if (i == list.size()) {
+                        shoppingTracker = j + 1;
+                        codeTracker ++;
+                        break;
+                    }
+                }
+                else{
+                    i = 0;
+                }
+                j++;
+            }
+
+            if(codeTracker > justhold) {
+                justhold++;
+                continue;
+            }
+            break;
+        }
+
+        if(codeTracker == codeList.size()){
+            return 1;
+        }
+
+        else return 0;
+
+    }
+
+    private static List<String> convertCorrectly(String element){  //converter of string to list
+        element = element.substring(1, element.length()-1);
+        List<String> list = new ArrayList<String>(Arrays.asList(element.split(", ")));
+        return list;
+    }
+}
---
+++ src/test/java/org/example/FooTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+public class FooTest {
+
+    @Test
+    public void testIsBuyerWinner1(){
+        List<String> codeList = new ArrayList<String>(Arrays.asList(""));
+        List<String> shoppingCart1 = new ArrayList<String>(Arrays.asList("apple", "apple", "banana", "banana", "orange", "banana"));
+
+        assertEquals(1, Foo.IsBuyerWinner(codeList, shoppingCart1));
+    }
+
+    @Test
+    public void testIsBuyerWinner2(){
+        List<String> codeList = new ArrayList<String>(Arrays.asList("[apple, apple]", "[banana, anything, banana]"));
+        List<String> shoppingCart1 = new ArrayList<String>(Arrays.asList("apple", "banana", "apple", "banana", "orange", "banana"));
+
+        assertEquals(0, Foo.IsBuyerWinner(codeList, shoppingCart1));
+    }
+
+    @Test
+    public void testIsBuyerWinner3(){
+        List<String> codeList = new ArrayList<String>(Arrays.asList("[apple, apple]", "[apple, apple, banana]"));
+        List<String> shoppingCart1 = new ArrayList<String>(Arrays.asList("apple", "apple", "apple", "banana"));
+
+        assertEquals(0, Foo.IsBuyerWinner(codeList, shoppingCart1));
+    }
+
+    @Test
+    public void testIsBuyerWinner4(){
+        List<String> codeList = new ArrayList<String>(Arrays.asList("[apple, apple]", "[banana, anything, banana]"));
+        List<String> shoppingCart1 = new ArrayList<String>(Arrays.asList("orange", "apple", "apple", "banana", "orange", "banana"));
+
+        assertEquals(1, Foo.IsBuyerWinner(codeList, shoppingCart1));
+    }
+
+    @Test
+    public void testIsBuyerWinner5(){
+        List<String> codeList = new ArrayList<String>(Arrays.asList("[apple, apple]", "[banana, anything, banana]"));
+        List<String> shoppingCart1 = new ArrayList<String>(Arrays.asList("orange", "apple", "apple","avocado", "banana", "orange", "banana"));
+
+        assertEquals(1, Foo.IsBuyerWinner(codeList, shoppingCart1));
+    }
+}
